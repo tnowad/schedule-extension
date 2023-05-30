64c17a56744d861522c9b8020dd0baf66218e67a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-30 07:29:01 +0700

    fix indxe

---
+++ src/pages/Popup.tsx
@@ -0,0 +1,3 @@
+export function Popup() {
+  return <div>Popup</div>;
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../assets/style.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>Setting</div>
+  </React.StrictMode>
+);
